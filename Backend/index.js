cda19a9f38315f5164c23aeff9b0fc500fe3dfa7
Mohd Shoaib <user.email like:"000shoaibsiddiqui@gmail.com>
2025-02-10 17:30:43 +0530

    signup started

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,27 @@
 const express=require("express");
-
+const cors=require("cors")
 const app=express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
 
 app.get("/",function(req,res){
     res.send("i am working")
 });
+
+
+app.post("/signup",function(req,res){
+
+const {firstname,lastname,email,number,password,cnfrmpassword}=req.body;
+
+
+   console.log(req.body)
+  
+})
+
 
 const Port=4000;
 
